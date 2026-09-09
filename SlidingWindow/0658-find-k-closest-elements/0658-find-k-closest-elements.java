e3c76afb22cfab2ebe00e21985a1fd36eb17c56c
Krishna Bajaj <krishnabajaj61206@gmail.com>
2026-09-10 01:00:23 +0530

    Time: 4 ms (97.27%), Space: 47.8 MB (65.91%) - LeetHub-Neo

---
+++ SlidingWindow/0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int left = 0;
+        int right = arr.length - 1;
+
+while (right - left >= k) {
+    if (Math.abs(arr[left] - x) > Math.abs(arr[right] - x)) {
+        left++;
+    } else {
+        right--;
+    }
+}
+
+ArrayList<Integer> ans = new ArrayList<>();
+
+for (int i = left; i <= right; i++) {
+    ans.add(arr[i]);
+}
+
+return ans;
+    }
+}
