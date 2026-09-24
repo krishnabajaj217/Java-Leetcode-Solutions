98f195c5fefcc563b55d9c6d912ffc2b3ce5c340
Krishna Bajaj <krishnabajaj61206@gmail.com>
2026-09-24 23:19:44 +0530

    Add Easy Java Solution for 1019. Squares of a Sorted Array from LeetCode

--- LeetCode/Patterns/Two Pointers/1019. Squares of a Sorted Array/Solution.java
+++ LeetCode/Patterns/Two Pointers/1019. Squares of a Sorted Array/Solution.java
@@ -1,13 +1,33 @@
-import java.util.Arrays;
+// import java.util.Arrays;
 
+// class Solution {
+//     public int[] sortedSquares(int[] nums) {
+//         // Step 1: Square each element in place
+//         for (int i = 0; i < nums.length; i++) {
+//             nums[i] = nums[i] * nums[i];
+//         }
+    
+//         Arrays.sort(nums);
+//         return nums;
+//     }
+// }
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        // Step 1: Square each element in place
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = nums[i] * nums[i];
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0, right = n - 1;
+        
+        // Fill the result array from right to left with the largest squares first
+        for (int i = n - 1; i >= 0; i--) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[i] = nums[right] * nums[right];
+                right--;
+            }
         }
-    
-        Arrays.sort(nums);
-        return nums;
+        
+        return result;
     }
 }
