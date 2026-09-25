7a96df0e7a36680c148e418fe7d04d7874438adc
Krishna Bajaj <krishnabajaj61206@gmail.com>
2026-09-25 23:47:08 +0530

    Add Medium Java Solution for 560. Subarray Sum Equals K from LeetCode

--- LeetCode/Java/Medium/560. Subarray Sum Equals K/README.md
+++ LeetCode/Java/Medium/560. Subarray Sum Equals K/README.md
@@ -8,8 +8,8 @@
 Array, Hash Table, Prefix Sum
 
 ### 🚀 Performance
-- **Runtime:** 1556 ms
-- **Memory:** 48.6 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Patterns/Prefix Sum/560. Subarray Sum Equals K/Solution.java
+++ LeetCode/Patterns/Prefix Sum/560. Subarray Sum Equals K/Solution.java
@@ -1,20 +1,39 @@
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+
+//         int count = 0;
+
+//         for (int i = 0; i < nums.length; i++) {
+
+//             int sum = 0;
+
+//             for (int j = i; j < nums.length; j++) {
+
+//                 sum += nums[j];
+
+//                 if (sum == k) {
+//                     count++;
+//                 }
+//             }
+//         }
+
+//         return count;
+//     }
+// }
 class Solution {
     public int subarraySum(int[] nums, int k) {
-
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
         int count = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-
-            int sum = 0;
-
-            for (int j = i; j < nums.length; j++) {
-
-                sum += nums[j];
-
-                if (sum == k) {
-                    count++;
-                }
+        for (int num : nums) {
+            sum += num;
+            int need = sum - k;
+            if (map.containsKey(need)) {
+                count += map.get(need);
             }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
 
         return count;
